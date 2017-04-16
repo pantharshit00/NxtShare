fcfdf19ebf491868a71ac4204ceec89e2580f85a
Harshit <pantharshit00@gmail.com>
2017-04-16 16:47:00 +0530

    Fixed spell mistake

--- dist/config/database.js
+++ dist/config/database.js
@@ -11,7 +11,7 @@ var _sequelize2 = _interopRequireDefault(_sequelize);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var connection = new _sequelize2.default(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DVB_HOST,
+    host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT
 });
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
 
 const connection = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
-    host:process.env.DVB_HOST,
+    host:process.env.DB_HOST,
     dialect:process.env.DB_DIALECT
 })
 
