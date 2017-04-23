d13b26c7a0c778d64f24f1092f2fc2f5690fa02d
Harshit <pantharshit00@gmail.com>
2017-04-23 23:14:59 +0530

    ported glyphicon from cdnjs

--- webpack.config.js
+++ webpack.config.js
@@ -15,14 +15,6 @@ module.exports = {
         {
             test: /\.scss?$/,
             loader: 'style-loader!css-loader!sass-loader'
-        },
-        {
-            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-        },
-        {
-            test: /\.(ttf|otf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?|(jpg|gif)$/,
-            loader: 'file-loader'
         }
         ]
     }
